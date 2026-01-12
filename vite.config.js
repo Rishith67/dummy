0d4230ac3d4a84e089d408b952959a7d6320c2cb
Rishith67 <rishithreddy69420@gmail.com>
2026-01-12 20:23:22 +0530

    first - intial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/dummy/', // Add this line to handle GitHub Pages subfolders
+})
